ae0e47ddc7fc0bf61c1bd4550bfaa3542b87b540
victor elaro <victorelaro123@gmail.com>
2025-04-23 14:04:40 -0300

    feat: criação da pagina de perfil de acesso, remoção dos specs e mudanças nas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PessoasComponent } from './pages/pessoas/pessoas.component';
 import { AuthGuard } from './guard/auth.guard';
+import { PerfisAcesso } from './pages/perfisAcesso/perfisAcesso.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent},
     { path: 'login', component: LoginComponent},
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
     { path: 'pessoas', component: PessoasComponent, canActivate: [AuthGuard]},
-    { path: 'ney', component: PessoasComponent, canActivate: [AuthGuard]}
+    { path: 'perfis-acesso', component: PerfisAcesso, canActivate: [AuthGuard]}
 ];
---
+++ src/app/pages/perfisAcesso/perfisAcesso.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+
+@Component({
+  selector: 'app-perfisAcesso',
+  imports: [MatTableModule],
+  templateUrl: './perfisAcesso.component.html',
+  styleUrl: './perfisAcesso.component.scss',
+})
+export class PerfisAcesso {
+  
+
+}
